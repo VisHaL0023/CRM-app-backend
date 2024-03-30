b78575f9d17d5f8f80adfc810a531077f18b2bdf
Vishal <vishalpanchal1828vp@gmail.com>
2024-03-31 02:41:17 +0530

    changed dbName

--- config/db.config.js
+++ config/db.config.js
@@ -2,5 +2,5 @@ require("dotenv").config();
 
 const mongoDbUri =
   process.env.MONGODB_URI || "mongodb://127.0.0.1:27017/crmApp";
-const dbName = "crmapp-db";
+const dbName = "crmApp";
 module.exports = { mongoDbUri, dbName };
