68ba395fe15da48eda2fb614ad84066908452710
Vishal <vishalpanchal1828vp@gmail.com>
2024-03-31 03:51:19 +0530

    last commit

--- index.js
+++ index.js
@@ -29,7 +29,12 @@ ticketRoutes(app);
 mongoose.connect(mongoDbUri, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
-  family: 4,
+  serverSelectionTimeoutMS: 5000,
+  autoIndex: false, // Don't build indexes
+  maxPoolSize: 10, // Maintain up to 10 socket connections
+  serverSelectionTimeoutMS: 5000, // Keep trying to send operations for 5 seconds
+  socketTimeoutMS: 45000, // Close sockets after 45 seconds of inactivity
+  family: 4, // Use IPv4, skip trying IPv6
 });
 const db = mongoose.connection;
 
